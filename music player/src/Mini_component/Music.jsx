6f5d93fc74239ae1ea4a2bcb01471f92f62d0d36
Rahul Srivastava <117383410+rahul-srivastava-codes@users.noreply.github.com>
2025-03-22 19:35:29 +0530

    Delete music player directory

--- music player/src/Mini_component/Music.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Music() {
-  return <div>music</div>;
-}
-
-export default Music;
--- music player/src/Mini_component/Podcast.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Podcast() {
-  return <div>podcast</div>;
-}
-
-export default Podcast;
